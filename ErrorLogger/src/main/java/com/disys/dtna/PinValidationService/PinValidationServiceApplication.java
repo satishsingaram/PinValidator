b19a5e1b31c119ca4b21a77f5318218797e4e7a7
satishsingaram <satish.singaram@gmail.com>
2020-08-27 23:25:07 +0530

    [DTNA] Intial Commmit

---
+++ ErrorLogger/src/main/java/com/disys/dtna/PinValidationService/PinValidationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.disys.dtna.PinValidationService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinValidationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PinValidationServiceApplication.class, args);
+	}
+
+}
---
+++ PinValidationService/src/test/java/com/disys/dtna/PinValidationService/PinValidationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.disys.dtna.PinValidationService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinValidationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
